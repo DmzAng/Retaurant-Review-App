e16603e31994b48f9aa64409794eca7ce47e4bef
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2024-10-18 14:30:17 +0800

    Second Commit : - Add new plugins - Update ViewModel - Update Function

--- app/src/main/java/com/dimaz/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/dimaz/restaurantreview/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dimaz.restaurantreview.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
@@ -11,24 +10,14 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.dimaz.restaurantreview.data.response.CustomerReviewsItem
-import com.dimaz.restaurantreview.data.response.PostReviewResponse
 import com.dimaz.restaurantreview.data.response.Restaurant
-import com.dimaz.restaurantreview.data.response.RestaurantResponse
-import com.dimaz.restaurantreview.data.retrofit.ApiConfig
 import com.dimaz.restaurantreview.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Callback
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
